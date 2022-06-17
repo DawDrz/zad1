62fabc40b090cc293a19d2d562b6217b566992e9
Dawid Drzewiecki <dawdrzew@gmail.com>
2022-06-17 19:44:09 +0200

    fix

--- bin/zad1.dart
+++ bin/zad1.dart
@@ -1,23 +1,22 @@
 import 'dart:io';
 
-// import 'package:dio/dio.dart' show Dio;
+import 'package:dio/dio.dart' show Dio;
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 final _router = Router()..get('/', _rootHandler);
 
-// final _dio = Dio();
+final _dio = Dio();
 
 Future<Response> _rootHandler(Request req) async {
   final connectionInfo =
       req.context['shelf.io.connection_info'] as HttpConnectionInfo;
   final ip = connectionInfo.remoteAddress.address;
-  // final response = await _dio.get("http://ipwho.is/$ip");
-  // final success = response.data["success"];
-  // if (!success) return Response.ok("$ip Adres prywatny!");
-  // final date = response.data['timezone']['current_time'];
-  final date = "";
+  final response = await _dio.get("http://ipwho.is/$ip");
+  final success = response.data["success"];
+  if (!success) return Response.ok("$ip Adres prywatny!");
+  final date = response.data['timezone']['current_time'];
   return Response.ok("$ip $date");
 }
 
